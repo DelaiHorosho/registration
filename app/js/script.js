558804f6ac9240f008efd927cc62e1b0b6f94e35
DelaiHorosho <Rik256@yandex.ru>
2017-03-04 15:53:55 +0300

    animation

--- app/js/script.js
+++ app/js/script.js
@@ -3,8 +3,20 @@ jQuery(document).ready(function() {
 
     controller.addTween('#fade-it', TweenMax.from( $('#fade-it'), 3, {css:{opacity: 0}}));
     controller.addTween('#fade-it-3', TweenMax.from( $('#fade-it-3'), 3, {css:{opacity: 0}}));
+    controller.addTween('#fade-it-4', TweenMax.from( $('#fade-it-4'), 3, {css:{opacity: 0}}));
+    controller.addTween('#fade-it-5', TweenMax.from( $('#fade-it-5'), 3, {css:{opacity: 0}}));
+    controller.addTween('#fade-it-6', TweenMax.from( $('#fade-it-6'), 3, {css:{opacity: 0}}));
+    controller.addTween('#fade-it-7', TweenMax.from( $('#fade-it-7'), 3, {css:{opacity: 0}}));
+    controller.addTween('#fade-it-8', TweenMax.from( $('#fade-it-8'), 3, {css:{opacity: 0}}));
+    controller.addTween('#fade-it-9', TweenMax.from( $('#fade-it-9'), 3, {css:{opacity: 0}}));
     controller.addTween('#fly-it', TweenMax.from( $('#fly-it'), 1, {css:{right:'1000px'}, ease:Quad.easeInOut}));
     controller.addTween('#fly-it-3', TweenMax.from( $('#fly-it-3'), 1, {css:{right:'1000px'}, ease:Quad.easeInOut}));
+    controller.addTween('#fly-it-4', TweenMax.from( $('#fly-it-4'), 1, {css:{right:'1000px'}, ease:Quad.easeInOut}));
+    controller.addTween('#fly-it-5', TweenMax.from( $('#fly-it-5'), 1, {css:{right:'1000px'}, ease:Quad.easeInOut}));
+    controller.addTween('#fly-it-6', TweenMax.from( $('#fly-it-6'), 1, {css:{right:'1000px'}, ease:Quad.easeInOut}));
+    controller.addTween('#fly-it-7', TweenMax.from( $('#fly-it-7'), 1, {css:{right:'1000px'}, ease:Quad.easeInOut}));
+    controller.addTween('#fly-it-8', TweenMax.from( $('#fly-it-8'), 1, {css:{right:'1000px'}, ease:Quad.easeInOut}));
+    controller.addTween('#fly-it-9', TweenMax.from( $('#fly-it-9'), 1, {css:{right:'1000px'}, ease:Quad.easeInOut}));
     controller.addTween('#spin-it', TweenMax.from( $('#spin-it'), .25, {css:{opacity:0, rotation: 720}, ease:Quad.easeOut}));
     controller.addTween('#scale-it', TweenMax.fromTo( $('#scale-it'), .25, {css:{opacity:0, fontSize:'20px'}, immediateRender:true, ease:Quad.easeInOut}, {css:{opacity:1, fontSize:'240px'}, ease:Quad.easeInOut}));
     controller.addTween('#smush-it', TweenMax.fromTo( $('#smush-it'), .25, {css:{opacity:0, 'letter-spacing':'30px'}, immediateRender:true, ease:Quad.easeInOut}, {css:{opacity:1, 'letter-spacing':'-10px'}, ease:Quad.easeInOut}), 0, 100); // 100 px offset for better timing
